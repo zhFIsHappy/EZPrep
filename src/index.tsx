53a241cfd8b1578a7faef280d16fde75c0f51a92
lyuran <lyuran@baidu.com>
2024-03-06 00:56:36 -0500

    adjust layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React, {createContext, useEffect, useState} from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {CategoryContextProvider} from "./contexts/CategoryContext";
-import {CartContextProvider} from "./contexts/CartContext";
+import React, { createContext, useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CategoryContextProvider } from "./contexts/CategoryContext";
+import { CartContextProvider } from "./contexts/CartContext";
 import "./assets/css/global.css";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        {/* <CategoryContextProvider>
+  <React.StrictMode>
+    {/* <CategoryContextProvider>
             <CartContextProvider> */}
-                <App/>
-            {/* </CartContextProvider>
+    <App />
+    {/* </CartContextProvider>
         </CategoryContextProvider> */}
-    </React.StrictMode>
+  </React.StrictMode>
 );
