2e8347392d14885ab4ad8f8aa997aa4763465ccd
zhfan901 <frank_f@infi.us>
2024-03-17 16:59:00 -0400

    edit api endpoint

--- src/apis/CodeEditorAPI.tsx
+++ src/apis/CodeEditorAPI.tsx
@@ -4,7 +4,7 @@ import { ServerError } from "../reducers/ServerError";
 const getProblemInfo = async () => {
   try {
     const response = await axios.get<ProblemInfo | ServerError>(
-      "https://ezprep.discovery.cs.vt.edu/api/get-problem-info"
+      "https://ezprep.discovery.cs.vt.edu/api/random-problem/easy"
     );
 
     console.log(response.data);
