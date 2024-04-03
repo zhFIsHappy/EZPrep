a128fba69ad502d173c35627fa9853eb640dbd5d
zhfan901 <frank_f@infi.us>
2024-04-02 22:08:44 -0400

    Implemented Register Form Validation

--- src/components/UserRegister.tsx
+++ src/components/UserRegister.tsx
@@ -1,76 +1,130 @@
-import {TextField} from "@mui/material";
+import { TextField } from "@mui/material";
 import Box from "@mui/material/Box";
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import Button from "@mui/material/Button";
-import LoadingButton from '@mui/lab/LoadingButton';
-import SendIcon from '@mui/icons-material/Send';
+import LoadingButton from "@mui/lab/LoadingButton";
+import SendIcon from "@mui/icons-material/Send";
+import Alert from "@mui/material/Alert";
 import axios from "axios";
 import { RegisterContext } from "../contexts/RegisterContext";
+import { registerSchema } from "../validations/RegisterValidations";
 
-
-const Register = ({onButtonClick}) =>{
-
-  const [ processing, setProcessing] = useState(false);
+interface registerForm {
+  name: "";
+  email: "";
+  password: "";
+  confirmPassword: "";
+}
+const Register = ({ onButtonClick }) => {
+  const [processing, setProcessing] = useState(false);
   const { registerForm, modifyForm } = useContext(RegisterContext);
-
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
   const handleFormModify = (e: any) => {
     const { name, value } = e.target;
     // console.log(name, value);
     modifyForm(name, value);
-  }
+  };
 
-  const submitRegister = () => {
+  const submitRegister = async (e) => {
     // axios.post()
     // TODO: make API request to register after backend finishing API building
-    console.log(registerForm);
+    // console.log(registerForm);
     setProcessing(true);
+    e.preventDefault();
+    const formData = {
+      name: registerForm.username,
+      email: registerForm.email,
+      password: registerForm.password,
+      confirmPassword: registerForm.repeatPassword,
+    };
+    try {
+      await registerSchema.validate(formData, { abortEarly: false });
+      console.log("Form Submitted", formData);
+    } catch (error: any) {
+      const newErrors: registerForm = {
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      };
+      if (error instanceof Error) {
+        error["inner"].forEach((err) => {
+          newErrors[err.path] = err.message;
+        });
+      }
+
+      setErrors(newErrors);
+    }
+    // const isValid = await registerSchema.isValid(formData);
+    // if (isValid) {
+    //   console.log("success");
+    // }
+    // const errorMessage = await registerSchema.validate(formData);
+
+    // alert(errorMessage);
+
     // onButtonClick("pagetwo");
-  }
+  };
 
   return (
     <>
-      <h1>
-        Step 1: Create an account
-      </h1>
+      <h1>Step 1: Create an account</h1>
       <div className="user-register-form">
         <Box
           component="form"
           display="flex"
           flexDirection="column"
           sx={{
-            '& .MuiTextField-root': { m: 1, width: '35ch' },
+            "& .MuiTextField-root": { m: 1, width: "35ch" },
           }}
           alignItems="center"
           autoComplete="off"
           justifyContent="center"
         >
           <TextField
+            error={errors.name.length != 0}
             name="username"
             label="User Name"
             type="text"
             value={registerForm.username}
             onChange={handleFormModify}
+            helperText={errors.name}
           />
+          {errors.name && <Alert severity="error">{errors.name}</Alert>}
           <TextField
+            error={errors.email.length != 0}
             name="email"
             label="Email"
             type="email"
             value={registerForm.email}
             onChange={handleFormModify}
+            helperText={errors.email}
           />
+        
+
           <TextField
+            error={errors.password.length != 0}
             name="password"
             label="Password"
             type="password"
             value={registerForm.password}
             onChange={handleFormModify}
+            helperText={errors.password}
           />
+
           <TextField
+            error={errors.confirmPassword.length != 0}
             name="repeatPassword"
             label="Confirm Password"
             type="password"
             value={registerForm.repeatPassword}
             onChange={handleFormModify}
+            helperText={errors.confirmPassword}
           />
         </Box>
       </div>
@@ -78,7 +132,7 @@ const Register = ({onButtonClick}) =>{
         <LoadingButton
           variant="contained"
           style={{ textTransform: "none" }}
-          onClick={ submitRegister }
+          onClick={submitRegister}
           loading={processing}
           loadingPosition="end"
           endIcon={<SendIcon />}
@@ -88,6 +142,6 @@ const Register = ({onButtonClick}) =>{
       </div>
     </>
   );
-}
+};
 
 export default Register;
